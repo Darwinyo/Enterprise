4871627f34804bf41e0b051f91568f64adccbab2
darwinyo <darwinyo91@gmail.com>
2017-08-18 15:44:04 +0700

    Add Documentation

--- Enterprise/Enterprise.Web.MPA/Enterprise.Web.MPA/Pages/ProductDetails/Index.cshtml.cs
+++ Enterprise/Enterprise.Web.MPA/Enterprise.Web.MPA/Pages/ProductDetails/Index.cshtml.cs
@@ -4,7 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Enterprise.Web.MPA.Models.ProductDetail
+using Enterprise.Web.MPA.Models.ProductDetail;
+using Enterprise.Web.MPA.BusinessLogics.StarRate;
 
 namespace Enterprise.Web.MPA.Pages.ProductDetails
 {
